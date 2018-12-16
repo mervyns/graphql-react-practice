7bdac783153deb2a902bf884a99286e1c0984fb1
mervyns <mervyns@gmail.com>
2018-12-17 01:52:17 +0800

    added mutation for create and delete

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -6,7 +6,8 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
-  GraphQLList
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
@@ -83,6 +84,49 @@ const RootQuery = new GraphQLObjectType({
   }
 });
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: {
+          type: new GraphQLNonNull(GraphQLString)
+        },
+        age: {
+          type: new GraphQLNonNull(GraphQLInt)
+        },
+        companyId: {
+          type: GraphQLString
+        }
+      },
+      resolve(parentValue, args) {
+        return axios.post(`http://localhost:3000/users`, {
+            firstName: args.firstName,
+            age: args.age,
+            companyId: args.companyId
+          })
+          .then(res => res.data);
+      }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQlString)
+        }
+      },
+      resolve(parentValue, {
+        id
+      }) {
+        return axios.delete(`http://localhost:3000/users/${id}`)
+          .then(res => res.data);
+      }
+    }
+  }
+});
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 })
