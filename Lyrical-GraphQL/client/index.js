2ad91b30d4853c4f43e4150ec7648001c96f509c
mervyns <mervyns@gmail.com>
2018-12-18 00:59:55 +0800

    Added Songlists create songs wired apollo etc

---
+++ Lyrical-GraphQL/client/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+import { ApolloProvider } from "react-apollo";
+import SongList from "./components/SongList";
+import SongCreate from "./components/SongCreate";
+import SongDetail from "./components/SongDetail";
+import App from "./components/App";
+
+const client = new ApolloClient({});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
